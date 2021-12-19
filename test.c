56011416fab71caef3246542501c25c84e33dd3f
Raven Jabha Joseph <ravenjabhajoseph@gmail.com>
2021-12-19 20:28:01 +0530

    Understanding the main merge concepts on the flow
    Learning commit ranges, double dot, Mulitple points.

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+	int alpha=10;
+	int beta=10;
+	int gamma=10;
+	int result=0;
+
+	result=alpha + beta + gamma;
+	printf("The result is: %d",result);
+}
+
